08b2d5a9d873aa7572d535b5c91220560b3c6627
Glauber Braga <Glauber Braga@MacBook>
2017-02-05 13:46:36 -0300

    Add prof e publicacoes

---
+++ Produtividade Academica/src/com/prod/acad/Professor.java
@@ -0,0 +1,14 @@
+package com.prod.acad;
+
+import java.util.ArrayList;
+
+public class Professor extends Colaborador{
+	
+	ArrayList<Colaborador> orientados;
+	
+	void Professor(){
+		orientados = new ArrayList<Colaborador>();
+	}
+	
+
+}
